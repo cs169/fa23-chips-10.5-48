cdff4435f89aaf5f3a332c3dfa2ff26d0c5b2c5a
Avinash Rao <avinashrao2002@berkeley.edu>
2023-11-20 20:43:23 +0000

    remove unneeded tests

--- spec/models/county_spec.rb
+++
@@ -1,7 +0,0 @@
-# require 'county'
-# describe County do
-#   let(:c) { County.new }
-#   it "basic" do  
-#     expect(120).to eq(120)
-#   end
-# end
--- spec/models/news_item_spec.rb
+++
@@ -1,6 +0,0 @@
-# describe NewsItem do
-#   let(:n) { NewsItem.new }
-#   it "basic" do  
-#     expect(120).to eq(120)
-#   end
-# end
