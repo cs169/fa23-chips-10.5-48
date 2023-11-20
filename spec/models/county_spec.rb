ce8493127266c37fd55dd83f87259aa38b1a10c8
Avinash Rao <avinashrao2002@berkeley.edu>
2023-11-20 20:40:02 +0000

    68% coverage

---
+++ spec/models/county_spec.rb
@@ -0,0 +1,7 @@
+# require 'county'
+# describe County do
+#   let(:c) { County.new }
+#   it "basic" do  
+#     expect(120).to eq(120)
+#   end
+# end
---
+++ spec/models/news_item_spec.rb
@@ -0,0 +1,6 @@
+# describe NewsItem do
+#   let(:n) { NewsItem.new }
+#   it "basic" do  
+#     expect(120).to eq(120)
+#   end
+# end
