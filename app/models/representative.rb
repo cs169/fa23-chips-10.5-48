a40da22f85adf24bab7edf0c14168e41c995c40b
Javier Pinedo <jepinedo555@berkeley.edu>
2023-11-24 17:57:31 +0000

    Styling for the profile page

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -9,9 +9,9 @@ class Representative < ApplicationRecord
     rep_info.officials.each_with_index do |official, index|
       ocdid_temp = ''
       title_temp = ''
-      # address_temp = ''
-      # pol_party_temp = ''
-      # photo_temp = ''
+      address_temp = ''
+      pol_party_temp = ''
+      photo_temp = ''
 
       rep_info.offices.each do |office|
         if office.official_indices.include? index
@@ -21,20 +21,20 @@ class Representative < ApplicationRecord
       end
 
       rep_info.officials.each do |person|
-        # adress_temp = person.address
-        # pol_party_temp = person.party
-        # photo_temp = person.photoUrl
+        adress_temp = person.address
+        pol_party_temp = person.party
+        photo_temp = person.photoUrl
       end
 
       # Can we make it so that when we create this representative, we also add in other information.  
       # This would probably be the easiest way to gather all the rep's information all in one method. 
       # For example:
-      #
-      # rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-      # title: title_temp,  address: adress_temp, polparty: pol_party_temp, photoUrl: photo_temp})
-
+      
       rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-          title: title_temp })
+      title: title_temp,  address: adress_temp, polparty: pol_party_temp, photoUrl: photo_temp})
+
+      # rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
+      #     title: title_temp })
 
       
       reps.push(rep)
