0d5b6943f418e96029fb336762b9b5eb1e4d2a19
Javier Pinedo <jepinedo555@berkeley.edu>
2023-11-24 19:36:48 +0000

    ran rubocop and corrected some mistakes, 0 lints detected

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -9,9 +9,6 @@ class Representative < ApplicationRecord
     rep_info.officials.each_with_index do |official, index|
       ocdid_temp = ''
       title_temp = ''
-      address_temp = ''
-      pol_party_temp = ''
-      photo_temp = ''
 
       rep_info.offices.each do |office|
         if office.official_indices.include? index
@@ -23,19 +20,10 @@ class Representative < ApplicationRecord
       address_temp = official.address
       pol_party_temp = official.party
       photo_temp = official.photoUrl
-      
 
-      # Can we make it so that when we create this representative, we also add in other information.  
-      # This would probably be the easiest way to gather all the rep's information all in one method. 
-      # For example:
-      
       rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-      title: title_temp,  address: address_temp, polparty: pol_party_temp, photoUrl: photo_temp})
+      title: title_temp, address: address_temp, polparty: pol_party_temp, photoUrl: photo_temp })
 
-      # rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-      #     title: title_temp })
-
-      
       reps.push(rep)
     end
 
